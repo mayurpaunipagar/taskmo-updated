d8f164515d3775c9cefdd4bf339d10668db5fa95
MayurHpNotebook <001mayur94@gmail.com>
2021-03-26 20:31:16 +0530

    added individual table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
+const url="http://103.212.121.222:3000/task/1137/report/details/new";
+function App(){
+  const [questions,setQuestions]=useState([]);
+  const [answers,setAnswers]=useState([]);
+
+  useEffect(()=>{
+    const fetchCall=async ()=>{
+      const res1=await fetch(url);
+      const res2=await res1.json()
+      const {task_questions,task_answers}=res2;
+      const allQuestions=task_questions.map(({question})=>question);
+
+      console.log(task_answers);
+      setQuestions([...allQuestions]);
+      setAnswers([...task_answers])
+    }
+    fetchCall();
+  },[])
+
+  return <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {answers.map(({user_id,task_id,answer})=>{
+        return <table>
+          <thead>
+            <th>User Id</th>
+            <th>Task Id</th>
+            {answer.map(({question})=>{
+              return <th>{question}</th>
+            })}
+          </thead>
+          <tbody>
+            <td>{user_id}</td>
+            <td>{task_id}</td>
+            {answer.map(({answer})=>{
+              return <td>{answer}</td>
+            })}
+          </tbody>
+        </table>
+      })}
     </div>
-  );
+  </>
+  
+  
 }
 
 export default App;
